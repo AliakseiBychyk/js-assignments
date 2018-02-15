9ba529b2f486f250714038ed34ab52cff5063bff
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-02-15 13:07:54 +0300

    typeof check

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -1,12 +1,9 @@
-'use strict';
-
 /** ******************************************************************************************
  *                                                                                          *
  * Plese read the following tutorial before implementing tasks:                             *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String  *
  *                                                                                          *
  ******************************************************************************************* */
-
 
 /**
  * Returns the result of concatenation of two strings.
@@ -20,10 +17,7 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-const concatenateStrings = (value1, value2) => (
-  value1 + value2
-);
-
+const concatenateStrings = (value1, value2) => value1 + value2;
 
 /**
  * Returns the length of given string.
@@ -36,10 +30,7 @@ const concatenateStrings = (value1, value2) => (
  *   'b'     => 1
  *   ''      => 0
  */
-const getStringLength = value => (
-  value.length
-);
-
+const getStringLength = value => value.length;
 
 /**
  * Returns the result of string template and given parameters firstName and lastName.
@@ -54,9 +45,7 @@ const getStringLength = value => (
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
-const getStringFromTemplate = (firstName, lastName) => {
-  return `Hello, ${firstName} ${lastName}!`;
-}
+const getStringFromTemplate = (firstName, lastName) => `Hello, ${firstName} ${lastName}!`;
 
 /**
  * Extracts a name from template string 'Hello, First_Name Last_Name!'.
@@ -68,10 +57,7 @@ const getStringFromTemplate = (firstName, lastName) => {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-const extractNameFromTemplate = value => {
-  return value.slice(7, -1);
-}
-
+const extractNameFromTemplate = value => value.slice(7, -1);
 
 /**
  * Returns a first char of the given string.
@@ -148,7 +134,6 @@ function unbracketTag(str) {
   return str.replace(/[<>]/g, '');
 }
 
-
 /**
  * Converts all characters of the specified string into the upper case
  *
@@ -170,7 +155,7 @@ function convertToUpperCase(str) {
  * @return {array}
  *
  * @example
- *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com' => 
+ *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com' =>
  *      ['angus.young@gmail.com', 'brian.johnson@hotmail.com', 'bon.scott@yahoo.com']
  *   'info@gmail.com' => ['info@gmail.com']
  */
@@ -202,15 +187,14 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-    return [...new Array(height).keys()].reduce((acc, el) => {
-        return el === 0
-            ? acc += ('┌' + '─'.repeat(width - 2) + '┐\n')
-            : el === height - 1 
-                ? acc += ('└' + '─'.repeat(width - 2) + '┘\n')
-                : acc += ('│' + ' '.repeat(width - 2) + '│\n')
-  }, '') 
+  return [...new Array(height).keys()].reduce(
+    (acc, el) =>
+      (el === 0
+        ? (acc += `┌${'─'.repeat(width - 2)}┐\n`)
+        : el === height - 1 ? (acc += `└${'─'.repeat(width - 2)}┘\n`) : (acc += `│${' '.repeat(width - 2)}│\n`)),
+    '',
+  );
 }
-
 
 /**
  * Encode specified string with ROT13 cipher
@@ -228,9 +212,13 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-    const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-  return 
+  const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+  const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'.split('');
+  const lookup = input.reduce((acc, el, i) => ({ ...acc, [el]: output[i] }), {});
+  return str
+    .split('')
+    .map(x => lookup[x] || x)
+    .join('');
 }
 
 /**
@@ -247,9 +235,10 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-  throw new Error('Not implemented');
+  return (
+    value !== null && (typeof value === 'string' || (typeof value === 'object' && typeof value.valueOf() === 'string'))
+  );
 }
-
 
 /**
  * Returns playid card id.
@@ -278,7 +267,6 @@ function isString(value) {
 function getCardId(value) {
   throw new Error('Not implemented');
 }
-
 
 export {
   concatenateStrings,
